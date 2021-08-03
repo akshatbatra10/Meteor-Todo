27d46e364769c9c64fad378e0492b3e6d756c794
akshatbatra10 <62565788+akshatbatra10@users.noreply.github.com>
2021-08-03 18:02:38 +0530

    added checkbox for completed task and delete task functionality

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -6,6 +6,18 @@ import { TaskForm } from './taskForm.jsx';
 
 export const App = () => {
 
+  const toggleChecked = () => {
+    TasksCollection.update(_id, {
+      $set: {
+        isChecked: !isChecked
+      }
+    });
+  }
+
+  const deleteTask = ({ _id }) => {
+    TasksCollection.remove(_id);
+  }
+
   const tasks = useTracker(() => {
     return TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch();
   });
@@ -15,7 +27,7 @@ export const App = () => {
       <h1>Welcome to Meteor!</h1>
       <TaskForm />
       <ul>
-        { tasks.map((task) => <Task key={task._id} task={task} />) }
+        { tasks.map((task) => <Task key={task._id} task={task} onCheckboxClick={toggleChecked} onDeleteClick={deleteTask} />) }
       </ul>
     </div>
     )
